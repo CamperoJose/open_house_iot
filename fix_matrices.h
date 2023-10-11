fe8feea6894fe52450652584889189427b2cc371
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2023-10-11 12:26:54 -0400

    Librería para arreglar el orden de las matrices

---
+++ fix_matrices.h
@@ -0,0 +1,18 @@
+#define N 10
+
+void reverseOddRows(int arr[][N], int n) {
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 1) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = arr[i][start];
+                arr[i][start] = arr[i][end];
+                arr[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
+
